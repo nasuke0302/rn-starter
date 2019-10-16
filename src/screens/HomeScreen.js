1ce48d0154fe8c504329963f9a346fa94eac3845
Alberto Esquivel <albertoesquivelanias@gmail.com>
2019-10-15 22:16:13 -0400

    added simple navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+
+const HomeScreen = props => {
+
+    return (
+        <View>
+            <Text style={styles.text}>Hello There</Text>
+            <Button 
+                onPress={() => props.navigation.navigate('Components')}
+                title='Go to Components Demo'
+            />
+            <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
+                <Text>Go to List Demo</Text>
+            </TouchableOpacity>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+    text: {
+        fontSize: 30
+    }
 });
 
 export default HomeScreen;
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
+    const data = [
+        { name: 'Albert', age: '25' },
+        { name: 'Nasuke', age: '25' },
+        { name: 'Alucard', age: '25' },
+        { name: 'Sasuke', age: '25' },
+        { name: 'Naruto', age: '25' },
+        { name: 'Sakura', age: '25' },
+    ];
+    const listItem = item => <Text style={styles.listItem}>{item.name} - Lvl. {item.age}</Text>
+
     return (
         <View>
             <Text>List Screen</Text>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                keyExtractor={el => el.name}
+                data={data}
+                renderItem={({ item }) => listItem(item)}
+            />
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-
+    listItem: {
+        margin: 5,
+        padding: 10,
+        backgroundColor: '#eee',
+        borderRadius: 10
+    }
 });
 
 export default ListScreen
