8cfbec2fcbe6a587ce22782fa5b67378d7407b0f
Alberto Esquivel <albertoesquivelanias@gmail.com>
2019-10-17 11:33:45 -0400

    added password input to Text screen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,18 +2,37 @@ import React, { useState } from 'react'
 import { Text, View, TextInput, StyleSheet } from 'react-native'
 
 export default () => {
-    const [text, setText] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const isValidPassword = password => password.length > 4 ? true : false;
+
     return (
         <View>
-            <Text>Enter name:</Text>
-            <TextInput
-                style={styles.textInput}
-                autoCapitalize="none"
-                autoCorrect={false}
-                value={text}
-                onChangeText={newValue => setText(newValue)}
-            />
-            <Text>Your name is {text}</Text>
+            <View style={styles.wrapper}>
+
+                <Text>Enter name:</Text>
+                <TextInput
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={name}
+                    onChangeText={newValue => setName(newValue)}
+                />
+                <Text>Your name is {name}</Text>
+            </View>
+            <View style={styles.wrapper}>
+                <Text>Enter password:</Text>
+                <TextInput
+                    style={styles.textInput}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={newValue => setPassword(newValue)}
+                />
+                <Text>{isValidPassword(password) ? 'valid password' : 'password must be 5 chars long'} </Text>
+            </View>
         </View>
     )
 }
@@ -25,5 +44,8 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: '#eee',
         borderRadius: 5,
+    },
+    wrapper: {
+        marginVertical: 15
     }
 });
